5445d52b8d8a4343091df62e78a6c49be3f7267e
jorgeavilasanchez <=>
2024-05-06 17:10:23 +0200

    actualiza 06052024

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-// Asegúrate de que este código se ejecute después de que la página haya cargado
 document.addEventListener('DOMContentLoaded', function() {
     // Inicialización de Swiper (asegúrate de que se inicialice correctamente)
     var swiper = new Swiper('.swiper-container', {
@@ -28,13 +27,27 @@ document.addEventListener('DOMContentLoaded', function() {
     headerTitle.addEventListener('mouseout', () => {
         headerTitle.classList.remove('highlight'); // Removemos la clase al quitar el cursor
     });
-});
 
-$(document).ready(function() {
-    $('#show-more-btn').click(function() {
-        $('.bio-container').toggleClass('expanded');
-        $(this).text(function(i, text) {
-            return text === 'Leer más' ? 'Leer menos' : 'Leer más';
+    // Inicialización de Typed.js si existe un elemento con la clase 'typed'
+    if ($('.typed').length) {
+        let typed_strings = $(".typed").data('typed-items');
+        typed_strings = typed_strings.split(',');
+        new Typed('.typed', {
+            strings: typed_strings,
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 2000
+        });
+    }
+
+    // Mostrar/ocultar texto al hacer clic en un botón
+    $(document).ready(function() {
+        $('#show-more-btn').click(function() {
+            $('.bio-container').toggleClass('expanded');
+            $(this).text(function(i, text) {
+                return text === 'Leer más' ? 'Leer menos' : 'Leer más';
+            });
         });
     });
 });
